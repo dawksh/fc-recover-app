a87f767a36cee190e7ac07150c683d40743a973b
daksh <akshd@outlook.com>
2024-03-08 17:42:25 +0530

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+    >
+      <Component {...pageProps} />
+    </NextThemesProvider>
+  );
 }
